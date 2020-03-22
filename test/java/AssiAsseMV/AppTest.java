0ae323b95598ae02e2740d49f915251fbcdbca2b
Ioana Joj <ioanna.gheorghiu@gmail.com>
2020-03-22 14:18:37 +0200

    Before and After test methods

--- test/java/AssiAsseMV/AppTest.java
+++ test/java/AssiAsseMV/AppTest.java
@@ -1,14 +1,30 @@
 package AssiAsseMV;
 
 import domain.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
 import validation.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 
 import static org.junit.Assert.*;
@@ -20,21 +36,50 @@ public class AppTest
 {
     public static final String SOURCES =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+    private static Service service;
+
+    private static void createXMLFile(String fileName) throws ParserConfigurationException, TransformerException
+    {
+        DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+        Document document = documentBuilder.newDocument();
+        Element root = document.createElement("Entitati");
+        document.appendChild(root);
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File(fileName));
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(domSource, streamResult);
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws TransformerException, ParserConfigurationException {
+        createXMLFile("test-studenti.xml");
+        createXMLFile("test-teme.xml");
+        createXMLFile("test-note.xml");
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "test-studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "test-teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "test-note.xml");
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    @AfterClass
+    public static void afterClass() throws IOException {
+        Files.delete(Paths.get("test-studenti.xml"));
+        Files.delete(Paths.get("test-teme.xml"));
+        Files.delete(Paths.get("test-note.xml"));
+    }
+
     /**
      * Rigorous Test :-)
      */
     @Test
     public void addStudentTC1()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         char[] textId = new char[10];
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
@@ -46,106 +91,42 @@ public class AppTest
     @Test(expected = ValidationException.class)
     public void addStudentTC2()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent("", "", 939));
     }
 
     @Test(expected = ValidationException.class)
     public void addStudentTC3()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent(null, null, 939));
     }
 
     @Test(expected = ValidationException.class)
     public void addStudentTC4()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent("", "", 109));
     }
 
     @Test(expected = ValidationException.class)
     public void addStudentTC5()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent(null, null, 109));
     }
 
     @Test(expected = ValidationException.class)
     public void addStudentTC6()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent(null, null, 110));
     }
 
     @Test(expected = ValidationException.class)
     public void addStudentTC7()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent("", "", 110));
     }
 
     @Test
     public void addStudentTC8()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-
         char[] textId = new char[10];
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
@@ -157,16 +138,6 @@ public class AppTest
     @Test
     public void addStudentTC9()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-
         char[] textId = new char[10];
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
@@ -178,30 +149,12 @@ public class AppTest
     @Test(expected = ValidationException.class)
     public void addStudentTC10()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent(null, null, 938));
     }
 
     @Test(expected = ValidationException.class)
     public void addStudentTC11()
     {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1, service.saveStudent("", "", 938));
     }
 
